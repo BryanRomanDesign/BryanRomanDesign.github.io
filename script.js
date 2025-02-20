3bd4c1836c35f7812f48fc51f506d5982099fdfa
Bryan O. Román Carril <bryano_dev@outlook.com>
2025-02-20 14:17:20 -0400

    Classes division

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-fetch("footer.html").then(response => response.text()).then(data => { document.getElementById("footer-container").innerHTML = data; });
+fetch("Sections/header.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("header-container").innerHTML = data;
+
+        // Now the burger button exists, add the event listener
+        const menuToggle = document.querySelector(".menu-toggle");
+        const navMenu = document.getElementById("nav-menu");
+
+        menuToggle.addEventListener("click", function () {
+            navMenu.classList.toggle("active");
+            menuToggle.classList.toggle("open"); // Toggle class for icon change
+
+            // Change icon when menu is open
+            if (menuToggle.classList.contains("open")) {
+                menuToggle.innerHTML = "<i class='bx bx-x'></i>"; // X icon
+            } else {
+                menuToggle.innerHTML = "<i class='bx bx-menu'></i>"; // Menu icon
+            }
+        });
+    });
+    
+fetch("Sections/socialbar.html").then(response => response.text()).then(data => { document.getElementById("social-bar-container").innerHTML = data; });
+fetch("Sections/footer.html").then(response => response.text()).then(data => { document.getElementById("footer-container").innerHTML = data; });
 
 
 window.history.scrollRestoration = 'auto';
-
-document.addEventListener("DOMContentLoaded", function () {
-    const menuToggle = document.querySelector(".menu-toggle");
-    const navMenu = document.getElementById("nav-menu");
-
-    menuToggle.addEventListener("click", function () {
-        navMenu.classList.toggle("active");
-        menuToggle.classList.toggle("open"); // Toggle class for icon change
-
-        // Change icon when menu is open
-        if (menuToggle.classList.contains("open")) {
-            menuToggle.innerHTML = "<i class='bx bx-x'></i>"; // X icon
-        } else {
-            menuToggle.innerHTML = "<i class='bx bx-menu'></i>"; // Menu icon
-        }
-    });
-});
